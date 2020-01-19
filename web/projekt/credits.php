d4724dc53b53c00dee117b6b142ba72c4f960a43
KacperK9 <kkarwot@sigma.ug.edu.pl>
2020-01-19 21:07:41 +0100

    podstrona autor

--- web/projekt/credits.php
+++ web/projekt/credits.php
@@ -2,9 +2,9 @@
 	include 'head.php';
 ?>
 
-<section>
-email
-github
+<section class="about-sec center-content">
+<p class="contact"><a href="mailto:karwot.kacper1@gmail.com">email</a></p>
+<p class="contact"><a href="https://github.com/kacperk9">github</a></p>
 </section>
 
 <?php
